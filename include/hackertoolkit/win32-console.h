c415b44cfdd56f478590d5e815e85dd3f49b36a9
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-12-27 20:58:21 +0100

    Switch to using std::freopen in win32-console

--- include/hackertoolkit/win32-console.h
+++ include/hackertoolkit/win32-console.h
@@ -14,24 +14,39 @@ namespace htk {
 class Win32Console {
 public:
     Win32Console() {
-        ::AllocConsole();
-        freopen_s(&m_stdoutFile, "CONOUT$", "w", stdout);
+        if (!::AllocConsole()) {
+            return;
+        }
+
+#ifdef _MSC_VER
+#pragma warning(push)
+// Disable the warning because as of c++20 freopen_s is not a part of <cstdio>
+#pragma warning(disable : 4996) // 'freopen': This function or variable may be unsafe. Consider using freopen_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+#endif
+        m_stdoutFile = std::freopen("CONOUT$", "w", stdout);
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
     }
 
     ~Win32Console() {
         if (m_stdoutFile) {
-            fclose(m_stdoutFile);
+            std::fclose(m_stdoutFile);
+            ::FreeConsole();
         }
-        ::FreeConsole();
     }
 
     Win32Console(const Win32Console&) = delete;
     Win32Console& operator=(const Win32Console&) = delete;
 
     Win32Console(Win32Console&& other) noexcept
-        : m_stdoutFile{ std::exchange(other.m_stdoutFile, nullptr) } {}
+        : m_stdoutFile{ std::exchange(other.m_stdoutFile, nullptr) }
+    {}
 
     Win32Console& operator=(Win32Console&& other) noexcept {
+        if (m_stdoutFile) {
+            std::fclose(m_stdoutFile);
+        }
         m_stdoutFile = std::exchange(other.m_stdoutFile, nullptr);
     }
 
